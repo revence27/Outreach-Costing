144e77c1a025d6f89b3230f7b0dde938c5ea307f
Revence Kalibwani <revence@1st.ug>
2011-11-11 18:35:57 +0300

    Costs. Let's begin.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Costs::Application.config.secret_token = 'c40215f8ad2f2df1b5bff3e4bad7d42d49cb91292df1d438e75afde51462833742f1eb3ab2959013ab3d00bfd10e953b372628a9383f472816e529e8fb18f724'
